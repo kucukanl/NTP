273e6bc574936f65645b294b1bd0bd96ed0f4e41
Anil <anil_kucuk16@erdogan.edu.tr>
2017-09-22 19:58:19 +0300

    dort metot

---
+++ src/deneme_1/Deneme_1.java
@@ -0,0 +1,33 @@
+/*
+ *
+ */
+package deneme_1;
+
+/**
+ *
+ * @author PC4
+ */
+public class Deneme_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static  int a=5;  
+    static  int b=4;
+    
+    
+    public static void main(String[] args) {
+        //int c=a+b;
+        System.out.println(topla(5,8));
+        
+    }
+     public static int topla(int a, int b){
+     
+         return a+b;
+         
+        
+        
+    }
+    
+}
---
+++ src/deneme_1/deneme_2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deneme_1;
+
+/**
+ *
+ * @author PC4
+ */
+public class deneme_2 {
+    
+   
+    
+}
